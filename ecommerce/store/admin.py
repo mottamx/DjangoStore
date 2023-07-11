7e6af536e0d1e277c1357558311a8c53148368ba
mottamx <cmottaa@gmail.com>
2023-07-11 14:31:56 -0600

    Linked urls from app to project

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from . models import Product, Category
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields={'slug':('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields={'slug':('title',)}
+
+
