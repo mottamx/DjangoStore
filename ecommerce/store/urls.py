2bdc72745395529cc7521befe4c6aa62bd9211ef
mottamx <cmottaa@gmail.com>
2023-07-13 14:49:32 -0600

    Single product template, categories template, reverse url

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns=[
 
     path('', views.store, name='store'),
+    path('product/<slug:product_slug>/', views.product_info, name='product-info'),
+    path('search/<slug:category_slug>/', views.list_category, name='list-category'),
 
 ]
