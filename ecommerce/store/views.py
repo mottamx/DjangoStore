bf172a1f07790e809e6bf06ca5ef378bef862ddb
mottamx <cmottaa@gmail.com>
2023-07-12 23:04:00 -0600

    Added Categories and Products models, tested basic functions in front base template and Admin backend

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from . models import Category, Product
+
+
 def store(request):
-    return render(request, 'store/store.html')
+
+    all_products = Product.objects.all()
+    context = {'all_products' : all_products}
+    return render(request, 'store/store.html', context)
+
+def categories(request):
+    all_categories = Category.objects.all()
+    return {'all_categories':all_categories}
